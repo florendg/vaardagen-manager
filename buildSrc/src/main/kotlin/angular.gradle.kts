aac6f41cdf9c37472f7a9b122d07e1dbbb079cc9
floren <florendg@gmail.com>
2024-01-28 09:04:20 +0100

    add node support

--- buildSrc/src/main/kotlin/angular.gradle.kts
+++ buildSrc/src/main/kotlin/angular.gradle.kts
@@ -1,18 +1,27 @@
 
 val angularBuild by tasks.registering(Exec::class) {
+    dependsOn(yarnInstall)
     group = "angular"
     commandLine("yarn", "ng", "build","--base-href", "/${project.name}/")
     workingDir("${project.layout.projectDirectory}")
 }
 
 val angularServe by tasks.registering(Exec::class) {
+    dependsOn(yarnInstall)
     group = "angular"
     commandLine("yarn", "ng", "serve")
     workingDir("${project.layout.projectDirectory}")
 }
 
 val angularTest by tasks.registering(Exec::class) {
+    dependsOn(yarnInstall)
     group = "angular"
     commandLine("yarn", "ng", "test")
+    workingDir("${project.layout.projectDirectory}")
+}
+
+val yarnInstall by tasks.registering(Exec::class) {
+    group = "angular"
+    commandLine("yarn", "install")
     workingDir("${project.layout.projectDirectory}")
 }
