325984ab3df369cebed9ca97b225435130e1adb5
floren <florendg@gmail.com>
2024-06-17 07:00:04 +0200

    Add primary key

--- vaardagen-persistence/src/main/resources/create_tables.sql
+++ vaardagen-persistence/src/main/resources/create_tables.sql
@@ -3,7 +3,7 @@ DROP TABLE  if exists TRIP_LOG;
 
 CREATE TABLE TRIP_LOG
 (
-    id                UUID default uuid_generate_v4(),
+    id                UUID default uuid_generate_v4() primary key,
     TRIP_NUMBER       INTEGER,
     DEPARTURE_HARBOUR VARCHAR(100),
     DEPARTURE_DATE    DATE,
