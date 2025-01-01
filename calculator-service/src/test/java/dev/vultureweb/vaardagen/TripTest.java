4d5bf4a0e3112348c53d96a29169006ab33e8f3b
floren <florendg@gmail.com>
2025-01-01 16:56:41 +0100

    fix test

--- calculator-service/src/test/java/dev/vultureweb/vaardagen/TripTest.java
+++ calculator-service/src/test/java/dev/vultureweb/vaardagen/TripTest.java
@@ -6,14 +6,18 @@ import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TripTest {
 
+  private static final String testUUID ="test_uuid_123";
+
   private final String expectedTripJson = """
       {
-        "id" : "231225",
+        "uuid" : "test_uuid_123",
+        "tripNumber" : "231225",
         "departurePort" : "departurePort",
         "arrivalPort" : "arrivalPort",
         "departureDate" : "2023-12-25",
@@ -23,7 +27,7 @@ public class TripTest {
 
   @Test
   void tripToJson() throws Exception {
-    Trip trip = new Trip("231225", "departurePort", "arrivalPort", LocalDate.of(2023, 12, 25), LocalDate.of(2023, 12, 25), 0);
+    Trip trip = new Trip(testUUID, "231225", "departurePort", "arrivalPort", LocalDate.of(2023, 12, 25), LocalDate.of(2023, 12, 25), 0);
     var JacksonContextResolver = new JacksonContextResolver();
     var mapper = JacksonContextResolver.getContext(ObjectMapper.class);
     var json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(trip);
